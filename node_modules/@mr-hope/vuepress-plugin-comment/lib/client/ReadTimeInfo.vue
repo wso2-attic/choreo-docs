e524a28b6a51eef3df38d330feea0fcb9e04a9a9
shanira14 <shanira14@gmail.com>
2021-02-24 16:36:27 +0530

    add pipeline file

---
+++ node_modules/@mr-hope/vuepress-plugin-comment/lib/client/ReadTimeInfo.vue
@@ -0,0 +1,13 @@
+<template>
+  <span
+    v-if="readtime"
+    class="read-time-info"
+    :aria-label="hint"
+    data-balloon-pos="down"
+  >
+    <TimeIcon />
+    <span v-text="readtime" />
+  </span>
+</template>
+
+<script src="./ReadTimeInfo" />
---
+++ node_modules/vuepress-plugin-md-enhance/lib/client/icons/LoadingIcon.vue
@@ -0,0 +1,56 @@
+<template>
+  <svg
+    style="enable-background: new 0 0 50 50"
+    viewBox="0 0 30 30"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect height="5" width="4" x="0" y="13">
+      <animate
+        attributeName="height"
+        begin="0s"
+        dur="0.6s"
+        repeatCount="indefinite"
+        values="5;21;5"
+      />
+      <animate
+        attributeName="y"
+        begin="0s"
+        dur="0.6s"
+        repeatCount="indefinite"
+        values="13; 5; 13"
+      />
+    </rect>
+    <rect height="5" width="4" x="10" y="13">
+      <animate
+        attributeName="height"
+        begin="0.15s"
+        dur="0.6s"
+        repeatCount="indefinite"
+        values="5;21;5"
+      />
+      <animate
+        attributeName="y"
+        begin="0.15s"
+        dur="0.6s"
+        repeatCount="indefinite"
+        values="13; 5; 13"
+      />
+    </rect>
+    <rect height="5" width="4" x="20" y="13">
+      <animate
+        attributeName="height"
+        begin="0.3s"
+        dur="0.6s"
+        repeatCount="indefinite"
+        values="5;21;5"
+      />
+      <animate
+        attributeName="y"
+        begin="0.3s"
+        dur="0.6s"
+        repeatCount="indefinite"
+        values="13; 5; 13"
+      />
+    </rect>
+  </svg>
+</template>
