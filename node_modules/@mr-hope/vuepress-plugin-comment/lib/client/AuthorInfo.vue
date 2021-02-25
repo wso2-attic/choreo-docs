b0ff9a2583122e80d70b2f08ed7d15eb8545dec4
Choreo CI Agent <choreo-cicd@wso2.com>
2021-02-25 16:58:02 +0000

    Updating documentation in 20210225.1

---
+++ node_modules/@mr-hope/vuepress-plugin-comment/lib/client/AuthorInfo.vue
@@ -0,0 +1,8 @@
+<template>
+  <span v-if="author" :aria-label="hint" data-balloon-pos="down">
+    <AuthorIcon />
+    <span v-text="author" />
+  </span>
+</template>
+
+<script src="./AuthorInfo" />
